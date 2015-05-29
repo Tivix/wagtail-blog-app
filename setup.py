0c7628d5d846a72d757aa2d801b9711867e8fcda
Sumit Chachra <sumit@tivix.com>
2015-05-28 17:03:23 -0700

    setup.py changes.

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 import os
-from setuptools import setup
+from setuptools import setup, find_packages
 
 README = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
 
@@ -7,11 +7,12 @@ README = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
-    name='wagtail_blog',
+    name='wagtail-blog-app',
     version='0.1.0',
-    packages=['wagtail_blog'],
+    packages=find_packages(),
     include_package_data=True,
-    description='Simple wagtail blog.',
+    keywords = "django wagtail blog app tivix",
+    description='Simple wagtail blog for your Django project.',
     long_description=README,
     install_requires=[
         'Django>=1.7.0',
