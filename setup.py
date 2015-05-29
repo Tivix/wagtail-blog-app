e436fbeec6fcf46cfed81f107ee7dd67a18e6c6c
Sumit Chachra <sumit@tivix.com>
2015-05-28 17:27:14 -0700

    Renamed template folder to wagtail_blog

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='wagtail-blog-app',
-    version='0.1.1',
+    version='0.1.2',
     packages=find_packages(),
     include_package_data=True,
     keywords = "django wagtail blog app tivix",
